e8f31e97f4e320832e64000be042315424a55f80
Natarajan <sri-sachin.natarajan@capgemini.com>
2024-02-24 12:10:48 +0530

    first commit

---
+++ ex1.java
@@ -0,0 +1,13 @@
+package guvipractice;
+import java.util.Scanner;
+public class ex1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		while(num<=50) {
+			System.out.println(num);
+		num++;
+		}
+	}
+}
+

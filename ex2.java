8d541b9246e8193d440753521db842bb572a4b11
Sachin <86413545+SriSachin21@users.noreply.github.com>
2024-04-19 18:18:00 +0530

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ex2 {
 
 	public static void main(String[] args) {
+		//Getting input from user
 		Scanner sc=new Scanner(System.in);
 		int num=sc.nextInt();
 		if(num%2==0) {
